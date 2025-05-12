60864930d856488c23875188c266451de1e6c07b
LukaAuda <luka.auda@etu-upsaclay.fr>
2025-05-12 16:23:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,97 +38,94 @@ int main() {
      assert(fabs(area - expectedArea) < 1e-6);
      cout << "Test 'area' réussi!" << endl;
  
-     // Test de la méthode 'translate'
+     // Test translation
      Point translation(3, 4);
      c.translate(translation);
      assert(fabs(c.center.x - 3) < 1e-6 && fabs(c.center.y - 4) < 1e-6);
      cout << "Test 'translate' réussi!" << endl;
  
-     // Test de la méthode 'resize'
+     // Test ratio
      double ratio = 2.0;
      c.resize(ratio);
      assert(fabs(c.radius - 10.0) < 1e-6);
      cout << "Test 'resize' réussi!" << endl;
  
-     // Test de la méthode 'equals'
+     // Test egalite
      Circle otherCircle(10.0, Point(3, 4));
      assert(c.equals(otherCircle));
      cout << "Test 'equals' réussi!" << endl;
  
-     // (Optionnel) Test visuel de 'draw' – affiche les points du cercle
-     // c.draw(); // Décommente pour visualiser les points du cercle
  
      cout << "Tous les tests du cercle ont réussi!" << endl;
 
     // Partie 2 : Tests sur le carre
-    // Création d’un carré avec les points A(0, 0) et C(2, 2) => carré de côté √2 * √2 = 2
+    // Création d’un carré 
     Point A(0, 0);
     Point C(2, 2);
     Square s(A, C);
 
-    // Test de la méthode 'side'
-    double side = s.side();  // diagonale = 2√2, côté = 2
+    // Test cotes
+    double side = s.side();  
     double expectedSide = 2.0;
     assert(fabs(side - expectedSide) < 1e-6);
     cout << "Test 'side' réussi!" << endl;
 
-    // Test de la méthode 'perimeter'
+    // Test perimetre
     double perimeter = s.perimeter();
     double expectedPerimeter = 4 * expectedSide;
     assert(fabs(perimeter - 8.0) < 1e-6);
     cout << "Test 'perimeter' réussi!" << endl;
 
-    // Test de la méthode 'area'
+    // Test aire
     double area2 = s.area();
     double expectedArea2 = expectedSide * expectedSide;
     assert(fabs(area2 - 4.0) < 1e-6);
     cout << "Test 'area2' réussi!" << endl;
 
-    // Test de la méthode 'center'
+    // Test centre
     Point center2 = s.center();
     assert(fabs(center2.x - 1.0) < 1e-6 && fabs(center2.y - 1.0) < 1e-6);
     cout << "Test 'center2' réussi!" << endl;
 
-    // Test de la méthode 'translate'
+    // Test translation
     Point translation2(1, -1);
     s.translate(translation2);
     assert(fabs(s.A.x - 1.0) < 1e-6 && fabs(s.A.y - -1.0) < 1e-6);
     assert(fabs(s.C.x - 3.0) < 1e-6 && fabs(s.C.y - 1.0) < 1e-6);
     cout << "Test 'translate2' réussi!" << endl;
 
-    // Test de la méthode 'resize'
-    s.resize(0.5);  // réduction à 50%
-    assert(fabs(s.side() - 1.0) < 1e-6);  // nouveau côté : 1
+    // Test resize
+    s.resize(0.5);  
+    assert(fabs(s.side() - 1.0) < 1e-6);  
     cout << "Test 'resize' réussi!" << endl;
 
-    // Test de la méthode 'rotate'
-    s.rotate(M_PI / 2);  // rotation de 90° autour du centre
-    // On teste simplement que le centre reste inchangé après rotation
+    // Test rotation
+    s.rotate(M_PI / 2);  
     Point newCenter = s.center();
     assert(fabs(newCenter.x - 2.0) < 1e-6 && fabs(newCenter.y == 0.0) < 1e-6);
     cout << "Test 'rotate' réussi!" << endl;
 
-    // Test de la méthode 'equals'
+    // Test egalite
     Square s2(s.A, s.C);
     assert(s.equals(s2));
     cout << "Test 'equals' réussi!" << endl;
 
-    // Test de la méthode 'inscribedCircle'
+    // Test inscribedCircle
     Circle inCircle = s.inscribedCircle();
     assert(fabs(inCircle.radius - s.side() / 2) < 1e-6);
     assert(fabs(inCircle.center.x - s.center().x) < 1e-6 && fabs(inCircle.center.y - s.center().y) < 1e-6);
     cout << "Test 'inscribedCircle' réussi!" << endl;
 
-    // Test de la méthode 'circumscribedCircle'
+    // Test circumscribedCircle
     Circle outCircle = s.circumscribedCircle();
-    double expectedRadius = sqrt(2) * (s.side() / 2);  // moitié de la diagonale
+    double expectedRadius = sqrt(2) * (s.side() / 2);  
     assert(fabs(outCircle.radius - expectedRadius) < 1e-6);
     cout << "Test 'circumscribedCircle' réussi!" << endl;
 
     cout << "Tous les tests du carré ont réussi!" << endl;
 
     // Partie 3 : Tests sur le triangle
-    // Création de trois points pour former un triangle
+    // Création points triangle
     Point P(0, 0);
     Point Q(3, 0);
     Point R(0, 4);
@@ -142,19 +139,19 @@ int main() {
     assert(fabs(perimeter3 - expectedPerimeter3) < 1e-6);
     cout << "Test 'perimeter3' réussi!" << endl;
 
-    // Test de la méthode 'area'
+    // Test aire
     double area3 = t.area();
-    double expectedArea3 = 6.0;  // Aire attendue : (0.5 * base * hauteur) = (0.5 * 3 * 4)
+    double expectedArea3 = 6.0;  
     assert(fabs(area3 - expectedArea3) < 1e-6);
     cout << "Test 'area3' réussi!" << endl;
 
-    // Test de la méthode 'center'
+    // Test centre
     Point center3 = t.center();
-    Point expectedCenter(1.0, 1.333333);  // Centre attendu : (1,1.33)
+    Point expectedCenter(1.0, 1.333333);  
     assert(fabs(center.x - expectedCenter.x) < 1e-6 && fabs(center.y - expectedCenter.y) < 1e-6);
     cout << "Test 'center3' réussi!" << endl;
 
-    // Test de la méthode 'translate'
+    // Test translation
     Point translation3(2, 3);
     t.translate(translation3);
     assert(fabs(t.A.x - 2) < 1e-6 && fabs(t.A.y - 3) < 1e-6);
@@ -162,7 +159,7 @@ int main() {
     assert(fabs(t.C.x - 2) < 1e-6 && fabs(t.C.y - 7) < 1e-6);
     cout << "Test 'translate' réussi!" << endl;
 
-    // Test de la méthode 'resize'
+    // Test resize
     double ratio3 = 0.5;
     t.resize(ratio3);
     assert(fabs(t.A.x - 1) < 1e-6 && fabs(t.A.y - 1.5) < 1e-6);
@@ -170,15 +167,15 @@ int main() {
     assert(fabs(t.C.x - 1) < 1e-6 && fabs(t.C.y - 4.0) < 1e-6);
     cout << "Test 'resize' réussi!" << endl;
 
-    // Test de la méthode 'rotate'
-    double angle = 90;  // Rotation de 90 degrés
+    // Test rotation
+    double angle = 90;  
     t.rotate(angle);
     assert(fabs(t.A.x + 1.5) < 1e-6 && fabs(t.A.y - 1) < 1e-6);
     assert(fabs(t.B.x - 1.5) < 1e-6 && fabs(t.B.y - 3) < 1e-6);
     assert(fabs(t.C.x - 1.0) < 1e-6 && fabs(t.C.y - 1.0) < 1e-6);
     cout << "Test 'rotate' réussi!" << endl;
 
-    // Affichage final
+    
     cout << "Tous les tests ont réussi!" << endl;
 
     return 0;
